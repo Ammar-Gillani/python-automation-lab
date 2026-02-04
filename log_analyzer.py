685780ca7526b18af526b324e35ba3ed87947ce1
Ammar-Gillani <syedammar1947@gmail.com>
2026-02-04 17:26:34 +0500

    Update log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,4 +1,10 @@
 file = open("sample_log.txt", "r")
 lines = file.readlines()
 file.close()
+error_count = 0
+for line in lines:
+  if "ERROR" in line:
+    error_count += 1
+    
 print("Total lines:", len(lines))
+print("Error lines:", error_count)
